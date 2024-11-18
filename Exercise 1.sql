ba9f398e5f0c26e080f92dd9c7abc24cc86de424
loadevent <loadevent.academy@gmail.com>
2024-11-18 11:39:29 +0200

    exercise 1 completed

---
+++ Exercise 1.sql
@@ -0,0 +1,18 @@
+--1
+SELECT * FROM Suppliers
+
+--2
+SELECT orderid, orderdate, requireddate, customerid, employeeid
+FROM Orders
+
+--3
+SELECT Firstname, lastname, employeeid
+FROM Employees
+
+--4 
+SELECT regiondescription
+FROM Region
+
+--5
+SELECT categoryid, categoryname, [description]
+FROM Categories
